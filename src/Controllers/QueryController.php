423961e2a44bcf87f73e15bd29d4402e55d61360
AllenGu <13793165851@163.com>
2017-09-08 12:37:58 +0800

    添加查询控制器

---
+++ src/Controllers/QueryController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bc021
+ * Date: 17-6-17
+ * Time: 下午2:02
+ */
+namespace Notadd\Cloud\Controllers;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Cloud\Handlers\DeleteAllHandler;
+
+/**
+ * Class QueryController
+ *
+ * @package Notadd\Cloud\Controllers
+ */
+class QueryController extends Controller
+{
+    /**
+     * @param \Notadd\Cloud\Handlers\DeleteAllHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function deleteAll(DeleteAllHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+}
