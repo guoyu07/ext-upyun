2c688f0443c89a21c6d080bb44e9c6dd5b488232
allengu93 <13793165851@163.com>
2017-09-16 23:45:53 +0800

    更新音频代码

--- src/Handlers/SetUpyunModeAudioHandler.php
+++ src/Handlers/SetUpyunModeAudioHandler.php
@@ -47,7 +47,7 @@ class SetUpyunModeAudioHandler extends Handler
         if ($data) {
             $this->withCode(200)->withData($data)->withMessage('修改设置成功!');
         } else {
-            $this->withCode(201)->withMessage('修改设置失败!');
+            $this->withCode(201)->withError('修改设置失败!');
         }
     }
 
